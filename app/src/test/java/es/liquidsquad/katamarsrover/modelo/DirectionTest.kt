0043f29100c1885289b38831629e3638cb26df97
david.vallejo.prieto <david.vallejo.prieto@accenture.com>
2017-12-14 13:38:32 +0100

    Test giro derecha

--- app/src/test/java/es/liquidsquad/katamarsrover/modelo/DirectionTest.kt
+++ app/src/test/java/es/liquidsquad/katamarsrover/modelo/DirectionTest.kt
@@ -19,9 +19,32 @@ class DirectionTest {
     fun turnRigthFromNorthIsEast() {
         //Arrange
         var sut = Direction.NORTH
-        assertEquals(Direction.EAST,sut.turnRigth())
         //Act
         //Assert
-
+        assertEquals(Direction.EAST,sut.turnRigth())
+    }
+    @Test
+    fun turnRigthFromEastIsSouth() {
+        //Arrange
+        var sut = Direction.EAST
+        //Act
+        //Assert
+        assertEquals(Direction.SOUTH,sut.turnRigth())
+    }
+    @Test
+    fun turnRigthFroMSouthIsWest() {
+        //Arrange
+        var sut = Direction.SOUTH
+        //Act
+        //Assert
+        assertEquals(Direction.WEST,sut.turnRigth())
+    }
+    @Test
+    fun turnRigthFroMWestIsNorth() {
+        //Arrange
+        var sut = Direction.WEST
+        //Act
+        //Assert
+        assertEquals(Direction.NORTH,sut.turnRigth())
     }
 }
